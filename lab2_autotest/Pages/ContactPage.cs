d3587f2d78e546ece88a20827d04497f6b46c032
outsludssht <sivitskaya.liza@gmail.com>
2025-04-19 12:15:27 +0300

    Lab #6 + previous errors fixed

--- lab2_autotest/Pages/ContactPage.cs
+++ lab2_autotest/Pages/ContactPage.cs
@@ -11,18 +11,16 @@ namespace lab2_autotest.Pages
     {
         private readonly IWebDriver driver = WebDriverSingleton.Driver;
 
+        private By EmailLocator => By.XPath("//li[@plerdy-tracking-id ='35448735101']");
+        private By PhoneLTLocator => By.XPath("//li[@plerdy-tracking-id ='50296369501']");
+        private By PhoneBYLocator => By.XPath("//li[@plerdy-tracking-id ='39744896801']");
+        private By SocialTextLocator => By.XPath("//li[@plerdy-tracking-id ='64965466401']");
+
         public void Open() => driver.Navigate().GoToUrl("https://en.ehu.lt/contact");
-
-        public string GetEmail() =>
-            driver.FindElement(By.XPath("//li[@plerdy-tracking-id ='35448735101']")).Text;
-
-        public string GetPhoneLT() =>
-            driver.FindElement(By.XPath("//li[@plerdy-tracking-id ='50296369501']")).Text;
-
-        public string GetPhoneBY() =>
-            driver.FindElement(By.XPath("//li[@plerdy-tracking-id ='39744896801']")).Text;
-
-        public string GetSocialText() =>
-            driver.FindElement(By.XPath("//li[@plerdy-tracking-id ='64965466401']")).Text;
+        public string GetEmail() => driver.FindElement(EmailLocator).Text;
+        public string GetPhoneLT() => driver.FindElement(PhoneLTLocator).Text;
+        public string GetPhoneBY() => driver.FindElement(PhoneBYLocator).Text;
+        public string GetSocialText() => driver.FindElement(SocialTextLocator).Text;
     }
+
 }
--- lab2_autotest/Tests/Tests.cs
+++ lab2_autotest/Tests/Tests.cs
@@ -1,9 +1,7 @@
-﻿using lab2_autotest.Pages;
+﻿using FluentAssertions;
 using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Interactions;
-using System;
+using lab2_autotest.Pages;
 
 namespace lab2_autotest.Tests
 {
@@ -13,13 +11,22 @@ namespace lab2_autotest.Tests
         [Test, Category("Navigation")]
         public void VerifyNavigationToAboutPage()
         {
+            Logger.Information("Starting test: VerifyNavigationToAboutPage");
+
             var main = new MainPage();
             main.Open();
-            main.ClickAbout();
+            Logger.Debug("Navigated to MainPage");
 
-            Assert.That(WebDriverSingleton.Driver.Url, Is.EqualTo("https://en.ehu.lt/about/"));
-            Assert.That(WebDriverSingleton.Driver.Title, Is.EqualTo("About"));
-            Assert.That(main.GetHeaderText(), Is.EqualTo("About"));
+            main.ClickAbout();
+            Logger.Debug("Clicked on About link");
+
+            var currentUrl = WebDriverSingleton.Driver.Url;
+            var headerText = main.GetHeaderText();
+
+            currentUrl.Should().Be("https://en.ehu.lt/about/", "URL should match the About page URL");
+            headerText.Should().Be("About", "Header text should be 'About'");
+
+            Logger.Information("Test VerifyNavigationToAboutPage completed");
         }
 
         [TestCase("study programs", "https://en.ehu.lt/?s=study+programs")]
@@ -27,34 +34,53 @@ namespace lab2_autotest.Tests
         [Test, Category("Search")]
         public void VerifySearchFunctionality(string query, string expectedUrl)
         {
+            Logger.Information("Starting test: VerifySearchFunctionality with query: {Query}", query);
+
             var main = new MainPage();
             main.Open();
-            main.Search(query);
+            Logger.Debug("Navigated to MainPage");
 
-            Assert.That(WebDriverSingleton.Driver.Url, Is.EqualTo(expectedUrl));
-            Assert.That(main.GetHeaderText(), Is.EqualTo("Search results"));
+            main.Search(query);
+            Logger.Debug("Performed search with query: {Query}", query);
+
+            var currentUrl = WebDriverSingleton.Driver.Url;
+            currentUrl.Should().Be(expectedUrl, "URL should match the expected search results URL");
+
+            Logger.Information("Test VerifySearchFunctionality passed for query: {Query}", query);
         }
 
         [Test, Category("Localization")]
         public void VerifyLanguage()
         {
+            Logger.Information("Starting test: VerifyLanguage");
+
             var main = new MainPage();
             main.Open();
-            main.SwitchLanguageToLT();
+            Logger.Debug("Navigated to MainPage");
 
-            Assert.That(WebDriverSingleton.Driver.Url, Is.EqualTo("https://lt.ehu.lt/"));
+            main.SwitchLanguageToLT();
+            Logger.Debug("Switched language to LT");
+
+            WebDriverSingleton.Driver.Url.Should().Be("https://lt.ehu.lt/", "URL should match the Lithuanian language page");
+
+            Logger.Information("Test VerifyLanguage passed");
         }
 
         [Test, Category("Content")]
         public void VerifyContactForm()
         {
+            Logger.Information("Starting test: VerifyContactForm");
+
             var contact = new ContactPage();
             contact.Open();
+            Logger.Debug("Navigated to Contact page");
 
-            Assert.That(contact.GetEmail(), Is.EqualTo("E-mail: franciskscarynacr@gmail.com"));
-            Assert.That(contact.GetPhoneLT(), Is.EqualTo("Phone (LT): +370 68 771365"));
-            Assert.That(contact.GetPhoneBY(), Is.EqualTo("Phone (BY): +375 29 5781488"));
-            Assert.That(contact.GetSocialText(), Is.EqualTo("Join us in the social networks: Facebook Telegram VK"));
+            contact.GetEmail().Should().Be("E-mail: franciskscarynacr@gmail.com", "Email should match the expected value");
+            contact.GetPhoneLT().Should().Be("Phone (LT): +370 68 771365", "Phone (LT) should match the expected value");
+            contact.GetPhoneBY().Should().Be("Phone (BY): +375 29 5781488", "Phone (BY) should match the expected value");
+            contact.GetSocialText().Should().Be("Join us in the social networks: Facebook Telegram VK", "Social text should match the expected value");
+
+            Logger.Information("Test VerifyContactForm passed");
         }
     }
 }
